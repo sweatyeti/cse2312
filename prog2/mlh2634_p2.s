4ad254a6c8ac67229bb9b6fbbd0855d23e7b98a3
MattH <mathias@thefaced.net>
2017-11-01 00:41:26 -0500

    building-out prog2 - successfully retrieving a num that is <= 999

--- prog2/mlh2634_p2.s
+++ prog2/mlh2634_p2.s
@@ -12,8 +12,14 @@ main:
     BL _getrand             @ get a random #
     MOV R1, R0
     
+    PUSH {LR} 
+    SUB SP, SP, #4
+    LDR R0, =test_str
+    BL printf                       
+    ADD SP, SP, #4
+    POP {LR}
 
-
+    B _exit
 
 _seedrand:
     PUSH {LR}               @ backup return address
@@ -26,21 +32,29 @@ _seedrand:
 _getrand:
     PUSH {LR}               @ backup return address
     BL rand                 @ get a random number
-    CMP R0, #999
-    BGT _fixrand            @ if the rand# is > 999, we need to fix it to be [0-999]
-    POP {PC}                @ return 
+    CMP R0, #1000
+    POPLT {PC}
+    MOV R1, R0
+    B _fixrand              @ if the rand# is >= 1000, we need to fix it to be [0-999]
 
-_fixrand:
-    AND R0, R0, #1023
-_fixloop:	
-    CMP R0, #999
-    POPLE {PC}
-    MOV R0, R0, LSR#1
-    B _fixloop
+_fixrand:                   @ this is a slightly-modified version of C.McMurrough's _mod_unsigned function
+    MOV R2, #1000           @ set limit for the rand# to [0-999]
+    MOV R0, #0              @ initialize return value
+    B _modloopcheck
+    _modloop:
+        ADD R0, R0, #1      @ increment R0
+        SUB R1, R1, R2      @ subtract R2 from R1
+    _modloopcheck:
+        CMP R1, R2          @ check for loop termination
+        BHS _modloop        @ continue loop if R1 >= R2
+    MOV R0, R1              @ move remainder to R0
+    POP {PC}                @ return
 
 _exit:
     MOV R7, #1              @ terminate syscall, 1
     SWI 0                   @ execute syscall
     
     
-@.data
+
+.data
+test_str: .asciz "%d\n"
