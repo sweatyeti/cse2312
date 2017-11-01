20bc4de60a994480266c67a692c8d324ceb850b9
MattH <mathias@thefaced.net>
2017-10-31 20:06:30 -0500

    building-out. added random # stuff

--- prog2/mlh2634_p2.s
+++ prog2/mlh2634_p2.s
@@ -9,7 +9,10 @@
 
 main:
     BL _seedrand            @ seed random number generator with current time
-    MOV R0, #0              @ initialize index variable
+    BL _getrand             @ get a random #
+    MOV R1, R0
+    
+
 
 
 _seedrand:
@@ -20,6 +23,20 @@ _seedrand:
     BL srand                @ seed the random number generator
     POP {PC}                @ return 
 
+_getrand:
+    PUSH {LR}               @ backup return address
+    BL rand                 @ get a random number
+    CMP R0, #999
+    BGT _fixrand            @ if the rand# is > 999, we need to fix it to be [0-999]
+    POP {PC}                @ return 
+
+_fixrand:
+    AND R0, R0, #1023
+_fixloop:	
+    CMP R0, #999
+    POPLE {PC}
+    MOV R0, R0, LSR#1
+    B _fixloop
 
 _exit:
     MOV R7, #1              @ terminate syscall, 1
